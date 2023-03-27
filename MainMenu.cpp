f0145c590d7668e39c36b6602eea9da7fdb2de22
atn3126 <92393048+atn3126@users.noreply.github.com>
2023-03-27 22:55:26 +0200

    Menu ( sans pause )
    en cours

---
+++ MainMenu.cpp
@@ -0,0 +1,68 @@
+#include "MainMenu.h"
+
+MainMenu::MainMenu()
+{
+	if (!font.loadFromFile("font/PKMN RBYGSC.ttf")) {
+		std::cout << "error";
+	}
+
+	//Play
+	mainMenu[0].setFont(font);
+	mainMenu[0].setFillColor(Color::White);
+	mainMenu[0].setString("Play");
+	mainMenu[0].setCharacterSize(70);
+	mainMenu[0].setPosition(305, 500);
+	//Options
+	mainMenu[1].setFont(font);
+	mainMenu[1].setFillColor(Color::White);
+	mainMenu[1].setString("Options");
+	mainMenu[1].setCharacterSize(70);
+	mainMenu[1].setPosition(305, 600);
+	//Exit
+	mainMenu[2].setFont(font);
+	mainMenu[2].setFillColor(Color::White);
+	mainMenu[2].setString("Exit");
+	mainMenu[2].setCharacterSize(70);
+	mainMenu[2].setPosition(305, 700);
+
+	MainMenuSelected = 0;
+}
+
+void MainMenu::draw(sf::RenderWindow& window)
+{
+	for (int i = 0; i < Max_main_menu; ++i) {
+		window.draw(mainMenu[i]);
+	}
+
+}
+
+void MainMenu::MoveUp()
+{
+	if (MainMenuSelected - 1 >= -1) {
+		mainMenu[MainMenuSelected].setFillColor(Color::White);
+
+		MainMenuSelected--;
+		if (MainMenuSelected == -1) {
+			MainMenuSelected = 2;
+		}
+		mainMenu[MainMenuSelected].setFillColor(Color::Yellow);
+	}
+}
+
+void MainMenu::MoveDown()
+{
+	if (MainMenuSelected + 1 >= 0) {
+		mainMenu[MainMenuSelected].setFillColor(Color::White);
+
+		MainMenuSelected++;
+		if (MainMenuSelected == 3) {
+			MainMenuSelected = 0;
+		}
+		mainMenu[MainMenuSelected].setFillColor(Color::Yellow);
+	}
+}
+
+MainMenu::~MainMenu()
+{
+
+}

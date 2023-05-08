974ad18c05b9d620b0c484974e8d09ff02233cba
Riseik <emaillard@gaming.tech>
2023-05-08 23:00:42 +0200

    switching scene

--- Game.h
+++ Game.h
@@ -9,12 +9,13 @@ public:
 
     void initVariables();
     void gameInit();
+    void Open();
     void gameLoop();
 
     bool dead = false;
 
 private:
-
+    int map[WINDOW_HEIGHT / 32][WINDOW_WIDTH / 32];
     int tiles[WINDOW_HEIGHT / 32][WINDOW_WIDTH / 32];
     int items[WINDOW_HEIGHT / 32][WINDOW_WIDTH / 32];
     int actualMapX, actualMapY = 0;
